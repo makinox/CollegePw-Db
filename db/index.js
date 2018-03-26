485722ddc798702dcf6eaab633d65a5a11a2059d
Jesus Bossa <jesusbossa@protonmail.com>
2018-03-26 14:43:54 -0500

    Herokumode

--- db/index.js
+++ db/index.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const mysql = require('mysql')
-const password = require('../config')
+// const password = require('../config')
 
 const connection = mysql.createConnection({
-  host: process.env.hostt || 'localhost',
+  host: process.env.hostt || '0.0.0.0',
   user: 'root',
-  password: process.env.password || password,
-  database: 'collegepw'
+  password: process.env.password || 'Jesusmakinoxal100%',
+  database: 'collegepw',
+  port: '3306'
 })
 
 module.exports = connection
